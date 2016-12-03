9a71b55ee3a36e1bce08bb78e36242559fa0abe9
sharkbound <laptopblaster@gmail.com>
2016-12-02 20:31:29 -0800

    First Commit

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+
+namespace LimitedItemCommand
+{
+    public class Config : IRocketPluginConfiguration 
+    {
+        public int SpawningLimit;
+
+        public void LoadDefaults()
+        {
+            SpawningLimit = 30;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.Core.Plugins;
+using Rocket.Core.Commands;
+
+namespace LimitedItemCommand
+{
+    public class Plugin : RocketPlugin<Config>
+    {
+        public static Plugin Instance;
+        protected override void Load()
+        {
+            Instance = this;
+            Log("LimitedItemCommand Has Loaded!");
+            Log("Current spawning limit: " + Configuration.Instance.SpawningLimit.ToString());
+        }
+
+        protected override void Unload()
+        {
+            Log("LimitedItemCommand Has Loaded!");
+        }
+
+        public override Rocket.API.Collections.TranslationList DefaultTranslations
+        {
+            get
+            {
+                return new Rocket.API.Collections.TranslationList
+                {
+                    {"giving_console", "Giving [{0}] x{1} of item {2} ({3})"},
+                    {"giving_failed", "Failed to find a item matching id or name '{0}'"}
+                };
+            }
+        }
+
+        public static void Log(string msg)
+        {
+            Rocket.Core.Logging.Logger.Log(msg);
+        }
+    }
+}
